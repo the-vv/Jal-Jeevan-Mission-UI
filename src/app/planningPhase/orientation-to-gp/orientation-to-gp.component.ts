d73c755fa159d9545ba7298e2bc11499b8a7000b
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2021-07-11 17:31:38 +0530

    before seting up add row form orietation to gp

--- src/app/planningPhase/orientation-to-gp/orientation-to-gp.component.ts
+++ src/app/planningPhase/orientation-to-gp/orientation-to-gp.component.ts
@@ -17,13 +17,9 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
   formdata: Application = {
     files: []
   };
+  filesToUpload: any[] = [];
   isAdmin: boolean = this.user.isAdmin;
   applicationForm!: FormGroup;
-  introductionFile: any = null;
-  InterDepartmentFile: any = null;
-  GpBoardMeetingFile: any = null;
-  jontAccountFile: any = null;
-  interDepartmentPhoto: any = null;
   submitting: boolean = false;
   submittedApplcations: Application[] = [];
   editingId: string = '';
@@ -68,17 +64,22 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
     this.applicationForm = this.formBuilder.group({
       introductionDate: [''],
       introductionNo: [''],
+      introductionMinutesIndex: '',
       iecPlanNo: [''],
       iecPlanAmount: [''],
       interDepartmentDate: [''],
       interDepartmentNo: [''],
+      interDepartmentMinutesIndex: '',
+      interDepartmentPhotoIndex: '',
       GpBoardMeetingDate: [''],
       GpBoardMeetingNo: [''],
+      GpBoardMeetingResolutionIndex: '',
       JointAccountNo: [''],
       jointAccountDate: [''],
       jointAccountBank: [''],
       jointAccountBranch: [''],
       jointAccointIFSC: [''],
+      jointAccountPassbookIndex: '',
       jointAccountSecretary: [''],
       jointAccountPresident: [''],
       jointAccointAddress: ['']
@@ -90,7 +91,7 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
     if (this.editingId.length > 0) {
       this.formdata._id = this.editingId
     }
-    if (!this.introductionFile && !this.InterDepartmentFile && !this.GpBoardMeetingFile && !this.jontAccountFile && !this.interDepartmentPhoto) {
+    if (!this.filesToUpload.length) {
       console.log('No attatchments, continuing');
       this.formdata.values = this.applicationForm.value;
       this.formdata.category = this.data.selectedDetails;
@@ -103,27 +104,17 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
     else {
       // console.log(this.agreementFile)
       let form: FormData = new FormData();
-      if (this.introductionFile && !this.introductionFile.fid) {
-        form.append('file1', this.introductionFile, 'introductionAttatchement.' + this.introductionFile.name.split('.')[this.introductionFile.name.split('.').length - 1]);
-      }
-      if (this.InterDepartmentFile && !this.InterDepartmentFile.fid) {
-        form.append('file2', this.InterDepartmentFile, 'interDepartmentAttatchment.' + this.InterDepartmentFile.name.split('.')[this.InterDepartmentFile.name.split('.').length - 1]);
-      }
-      if (this.GpBoardMeetingFile && !this.GpBoardMeetingFile.fid) {
-        form.append('file3', this.GpBoardMeetingFile, 'gpBoardMeetingAttatchment.' + this.GpBoardMeetingFile.name.split('.')[this.GpBoardMeetingFile.name.split('.').length - 1]);
-      }
-      if (this.jontAccountFile && !this.jontAccountFile.fid) {
-        form.append('file4', this.jontAccountFile, 'jointAccountAttatchment.' + this.jontAccountFile.name.split('.')[this.jontAccountFile.name.split('.').length - 1]);
-      }
-      if (this.interDepartmentPhoto && !this.interDepartmentPhoto.fid) {
-        form.append('file4', this.interDepartmentPhoto, 'interDepartmentPhoto.' + this.interDepartmentPhoto.name.split('.')[this.interDepartmentPhoto.name.split('.').length - 1]);
-      }
+      this.filesToUpload.forEach(f => {
+        if (!f.file.fid) {
+          form.append(`Attatchement-${f.fname}`, f.file, `Attatchement-${f.fname}.` + f.file.name.split('.')[f.file.name.split('.').length - 1]);
+        }
+      })
       this.submitting = true;
       this.rest.uploadFiles(form)
         .subscribe((res) => {
           console.log(res)
           res.forEach((element: any) => {
-            let name = element.name.split('_')[0];
+            let name = element.name.split('_')[0].split('-')[1];
             this.formdata.files?.push({
               name: element.name,
               fieldName: name,
@@ -150,7 +141,7 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
           }
           else {
             this.submitting = false;
-            this.snackBar.open('Error uploading file, Please try again later', 'Dismiss', { duration: 5000 })
+            this.snackBar.open('Error uploading file(s), Please try again later', 'Dismiss', { duration: 5000 })
           }
         })
     }
@@ -158,21 +149,30 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
 
   fileSelected(event: any, name: string) {
     this.submitted = false;
-    console.log(event.files)
+    // console.log(event.files)
+    this.filesToUpload.push({
+      fname: `${name}`,
+      file: event.files[0]
+    });
     if (name === 'introductionAttatchement') {
-      this.introductionFile = event.files[0]
+      // this.introductionFile = event.files[0];
+      this.applicationForm.patchValue({ introductionMinutesIndex: `${name}` })
     }
     else if (name === 'interDepartmentAttatchment') {
-      this.InterDepartmentFile = event.files[0]
+      // this.InterDepartmentFile = event.files[0]
+      this.applicationForm.patchValue({ interDepartmentMinutesIndex: `${name}` })
     }
     else if (name === 'gpBoardMeetingAttatchment') {
-      this.GpBoardMeetingFile = event.files[0]
+      // this.GpBoardMeetingFile = event.files[0]
+      this.applicationForm.patchValue({ GpBoardMeetingResolutionIndex: `${name}` })
     }
     else if (name === 'jointAccountAttatchment') {
-      this.jontAccountFile = event.files[0]
+      // this.jontAccountFile = event.files[0]
+      this.applicationForm.patchValue({ jointAccountPassbookIndex: `${name}` })
     }
     else if (name === 'interDepartmentPhoto') {
-      this.interDepartmentPhoto = event.files[0]
+      // this.interDepartmentPhoto = event.files[0]
+      this.applicationForm.patchValue({ interDepartmentPhotoIndex: `${name}` })
     }
   }
 
@@ -186,11 +186,6 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
             this.showForm = false;
             this.editingId = '';
             this.applicationForm.reset()
-            this.introductionFile = null;
-            this.GpBoardMeetingFile = null;
-            this.InterDepartmentFile = null;
-            this.jontAccountFile = null;
-            this.interDepartmentPhoto = null;
             this.formdata.files = []
           }
           this.submittedApplcations = this.submittedApplcations.filter(el => {
@@ -215,11 +210,6 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
             this.showForm = false;
             this.editingId = '';
             this.applicationForm.reset()
-            this.introductionFile = null;
-            this.GpBoardMeetingFile = null;
-            this.InterDepartmentFile = null;
-            this.jontAccountFile = null;
-            this.interDepartmentPhoto = null;
             this.formdata.files = []
           }
           this.submittedApplcations.unshift(res);
@@ -234,63 +224,79 @@ export class OrientationToGpComponent implements OnInit, AfterViewInit {
     }
   }
 
-  fileRemoved(id: string) {
+  fileRemoved(name: string, fid: string) {
     this.submitted = false;
-    if (id && id.length > 0) {
+    this.filesToUpload = this.filesToUpload.filter(el => {
+      console.log(el, name)
+      return el.fname != `${name}`
+    });
+    // this.applicationForm.get('contribution')['controls'][index].value.bankIndex = '';
+    if (name === 'introductionAttatchement') {
+      // this.introductionFile = event.files[0];
+      this.applicationForm.patchValue({ introductionMinutesIndex: `` })
+    }
+    else if (name === 'interDepartmentAttatchment') {
+      // this.InterDepartmentFile = event.files[0]
+      this.applicationForm.patchValue({ interDepartmentMinutesIndex: `` })
+    }
+    else if (name === 'gpBoardMeetingAttatchment') {
+      // this.GpBoardMeetingFile = event.files[0]
+      this.applicationForm.patchValue({ GpBoardMeetingResolutionIndex: `` })
+    }
+    else if (name === 'jointAccountAttatchment') {
+      // this.jontAccountFile = event.files[0]
+      this.applicationForm.patchValue({ jointAccountPassbookIndex: `` })
+    }
+    else if (name === 'interDepartmentPhoto') {
+      // this.interDepartmentPhoto = event.files[0]
+      this.applicationForm.patchValue({ interDepartmentPhotoIndex: `` })
+    }
+    if (fid?.length) {
       console.log(this.formdata)
       this.formdata._id = this.editingId
+      this.formdata.values = this.applicationForm.value
       this.formdata.files = (this.formdata.files as ApplicationFile[]).filter(el => {
-        console.log(el.fid, id)
-        return el.fid != id
+        return el.fieldName != `${name}`
       })
       console.log(this.formdata)
-      this.sendApplication(this.formdata, true, true)
-      this.rest.deleteFile(id)
-        .subscribe((res) => {
-          console.log('file deleted', res)
-        }, err => console.log(err.error))
+      fid?.length > 0 && this.sendApplication(this.formdata, true, true)
+      if (fid) {
+        this.rest.deleteFile(fid)
+          .subscribe((res) => {
+            console.log('file deleted', res)
+          }, err => console.log(err.error))
+      }
     }
   }
 
   applicSelected(app: Application) {
+    this.onReset();
     this.showForm = true
     this.applicationForm.patchValue(app.values);
     this.editingId = app._id as string
     if ((app.files as ApplicationFile[])?.length > 0) {
-      (app.files as ApplicationFile[])?.forEach(el => {
-        if (el.fieldName === 'introductionAttatchement') {
-          this.introductionFile = el;
-          this.formdata.files?.push(el)
-        }
-        if (el.fieldName === 'interDepartmentAttatchment') {
-          this.InterDepartmentFile = el;
-          this, this.formdata.files?.push(el)
-        }
-        if (el.fieldName === 'gpBoardMeetingAttatchment') {
-          this.GpBoardMeetingFile = el;
-          this, this.formdata.files?.push(el)
-        }
-        if (el.fieldName === 'jointAccountAttatchment') {
-          this.jontAccountFile = el;
-          this.formdata.files?.push(el)
-        }
-        if (el.fieldName === 'interDepartmentPhoto') {
-          this.interDepartmentPhoto = el;
-          this.formdata.files?.push(el)
-        }
+      (app.files as ApplicationFile[])?.forEach(f => {
+        this.filesToUpload.push({
+          fname: f.fieldName,
+          file: f
+        })
       })
-    }
+      this.formdata.files = app.files
+    }    
   }
+  
+  getFileFromName(name: string) {
+    return this.filesToUpload.filter(el => {
+      return el.fname == `${name}`
+    })[0]
+  }
+
 
   onReset() {
     this.showForm = false;
     this.editingId = '';
     this.applicationForm.reset()
-    this.introductionFile = null;
-    this.GpBoardMeetingFile = null;
-    this.InterDepartmentFile = null;
-    this.jontAccountFile = null;
-    this.interDepartmentPhoto = null;
+    this.filesToUpload = [];
     this.formdata.files = []
   }
 
