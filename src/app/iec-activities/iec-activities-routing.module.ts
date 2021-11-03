1204147f1e318e7d0c9114ce99cd5931bbb1ac87
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2021-11-04 00:45:38 +0530

    hoarding and banners done

--- src/app/iec-activities/iec-activities-routing.module.ts
+++ src/app/iec-activities/iec-activities-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HoardingsBannersComponent } from './hoardings-banners/hoardings-banners.component';
 import { IecActivitiesComponent } from './iec-activities.component';
 import { WallPaintingsComponent } from './wall-paintings/wall-paintings.component';
 
 const routes: Routes = [
   { path: '', component: IecActivitiesComponent },
   { path: 'wall-paintings', component: WallPaintingsComponent },
+  { path: 'hoardings-banners', component: HoardingsBannersComponent },
 
 ];
 
